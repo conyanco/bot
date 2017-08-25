f15252659a8353cecd8b1e2a8da58dc2e92477a8
conyanco <n.namatame@gmail.com>
2017-08-25 23:38:13 +0900

    main

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 'use strict';
 
- apiai = require('apiai');
-
+apiai = require('apiai');
 
 const app = apiai("a558192b4a614b079e4fb86afb610e47");
-
-canst reqapi = app.textRequest('weather.condition', {
-    sessionId: '7cce81fc71054378954c46ffeb8e84ff'
+const reqapi = app.textRequest('weather.condition', {
+	sessionId: '7cce81fc71054378954c46ffeb8e84ff'
 });
 
-reqapi.on('response', function(response) {
-    console.log(response);
-});
+request.on('response', function(response) {
+	console.log(response);
+	});
 
-
-reqapi.on('error', function(error) {
+request.on('error', function(error) {
     console.log(error);
-});
+    });
 
-
-reapi.end();
+request.end();
